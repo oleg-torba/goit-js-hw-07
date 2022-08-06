00edc83c6daf068e44913ae2cdc273132b52578a
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-07 00:13:43 +0300

    some

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,24 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector(".gallery");
+const galleryList = createGalleryList(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", galleryList);
+
+function createGalleryList(galleryItems) {
+  return galleryItems
+  .map(({ preview, original, description }) => {
+    return `
+        <div class="gallery__item">
+    <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>`;
+  }).join("")
+}
