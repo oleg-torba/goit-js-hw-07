100b69ea61b607ada84f010a6ec6ffede65a3881
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-07 16:39:49 +0300

    1 complete

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,16 +1,16 @@
 import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+
 
 const galleryBox = document.querySelector(".gallery");
-const galleryList = createGalleryList(galleryItems);
-galleryBox.insertAdjacentHTML("beforeend", galleryList);
+const galleryList = createGalleryLightBox(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", galleryList)
 
-function createGalleryList(galleryItems) {
+function createGalleryLightBox(galleryItems) {
   return galleryItems
-  .map(({ preview, original, description }) => {
-    return `
-        <div class="gallery__item">
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
     <a class="gallery__link" href="${original}">
     <img
       class="gallery__image"
@@ -20,5 +20,19 @@ function createGalleryList(galleryItems) {
     />
   </a>
 </div>`;
-  }).join("")
+    })
+    .join("");
 }
+galleryBox.addEventListener("click", onGalleryLightBoxClick);
+function onGalleryLightBoxClick(e){
+    e.preventDefault();
+    const imgSource = e.target.dataset.source;
+    const instance = basicLightbox.create(`
+    <img
+    src="${imgSource}"
+    >
+`)
+
+instance.show()
+}
+
