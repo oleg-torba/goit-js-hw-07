14bd31e8b3b1a3ee9993529fb5c9e0bb5a5ac918
oleg-torba <100319055+oleg-torba@users.noreply.github.com>
2022-08-08 21:32:53 +0300

    02

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,34 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const galleryBox = document.querySelector(".gallery");
+const galleryList = createGalleryLightBox(galleryItems);
+galleryBox.insertAdjacentHTML("beforeend", galleryList);
+
+function createGalleryLightBox(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" />
+</a>`;
+    })
+    .join("");
+}
+
+const GalleryLightBox = new SimpleLightBox(".gallery a", {
+  overlayOpacity: 0.5,
+  captionDelay: 250,
+
+  captionsData: "alt",
+  captionPosition: "bottom",
+
+  animationSpeed: 250,
+
+  enableKeyboard: true,
+
+  disableRightClick: true,
+  disableScroll: false,
+  alertError: true,
+  alertErrorMessage: "Image not found, next image will be loaded",
+  additionalHtml: false,
+});
